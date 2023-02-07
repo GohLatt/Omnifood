3a03cd2a407ae12d11e61a52265c2acc02068cf7
GohLatt <116095270+GohLatt@users.noreply.github.com>
2023-02-07 08:05:10 +0630

    final

---
+++ Omifood/script/script.js
@@ -0,0 +1,67 @@
+document.querySelector(".icon-mobile-nav").addEventListener("click", () => {
+  document.querySelector(".head").classList.add("nav-open");
+});
+document.querySelector(".outline").addEventListener("click", () => {
+  document.querySelector(".head").classList.remove("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const selectEl = document.querySelector(href);
+      selectEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+//sticky
+const headEl = document.querySelector(".hero-section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //in the view port
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px", //sticky height
+  }
+);
+obs.observe(headEl);
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
